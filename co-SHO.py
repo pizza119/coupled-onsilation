466a5faf4dd2e1eca83c098d8e3d084c39158785
SeangBhin Yang <did030704@naver.com>
2023-05-03 15:58:03 +0900

    기본 창 설정하기

---
+++ co-SHO.py
@@ -0,0 +1,22 @@
+import pygame
+import numpy as np
+
+size1 = [600,600]
+screen = pygame.display.set_mode(size1)
+
+title = "co-SHO"
+pygame.display.set_caption(title)
+clock = pygame.time.Clock()
+SB = 0
+
+
+while SB == 0:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            SB = 1
+
+
+    pygame.display.update()
+#pygame.quit()
